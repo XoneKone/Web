caac6ac88bf36afd033f286a27865697ffa10cdd
XoneK <mr.blackmarch@mail.ru>
2021-04-06 13:47:17 +0300

    Cataract

--- My website/scripts/cataract.js
+++ My website/scripts/cataract.js
@@ -1,6 +1,20 @@
+function getRandomInt(min, max) {
+   min = Math.ceil(min);
+   max = Math.floor(max);
+   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+ }
+
 
 setInterval(function() {
-   let items = document.body.childNodes;
-   let item = items[Math.floor(Math.random() * items.length)];
-   item.setAttribute("visibility","hidden");
-  },120);
+   let items = document.getElementsByTagName("div");
+   
+   for(i = 0;i < items.length; i++){
+      items[i].style.visibility ='visible'
+   }
+
+   let randitem = getRandomInt(0,items.length);
+
+   items[randitem].style.visibility ="hidden"
+   
+  },500);
+
